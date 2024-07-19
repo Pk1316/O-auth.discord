ff3b810d852a940f878ce69be8d28abbc88cf493
pavan <p.pavankumar1306@gmail.com>
2024-07-19 14:17:17 +0530

    commit

--- src/stratagies/discord-strategy.mjs
+++ src/stratagies/discord-strategy.mjs
@@ -1,15 +1,16 @@
 import passport from 'passport'
 import { Strategy } from 'passport-discord'
 import{DiscordUser} from "../mongoose/discord-user.mjs"
-
+import dotenv from 'dotenv'
+dotenv.config();
 passport.serializeUser((user,data)=>{
     console.log(user)
     document(null,user.id)
 })
 export default passport.use(
     new Strategy({
-        clientID:"1245247016758546474",
-        clientSecret:"64cc6bc43d58c2ab7a1cbecdcf33a0c0b4f4d9b9983f742f17426a0df0431b71",
+        clientID:process.env.clientID,
+        clientSecret:process.env.clientSecret,
         callbackURL:"http://localhost:8000/api/auth/discord/redirect",
         scope:["identify","guilds"],
     },
